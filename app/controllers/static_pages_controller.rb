9ac7b149deefdb18cd3de1dce6cef2e01cca2ed0
chiNguyenTrex <xspacechinguyen@gmail.com>
2017-07-28 13:07:53 +0700

    static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+    # Do some thing here
+  end
+
+  def help
+    # Do some thing here
+  end
+
+  def about
+    # Do some thing here
+  end
+
+  def contact
+    # Do some thing here
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 
-class ActiveSupport
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
