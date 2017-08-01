ce948ceba9d9523c7181c3da6ff79c78ff6e55ca
chiNguyenTrex <xspacechinguyen@gmail.com>
2017-08-01 15:56:35 +0700

    sign up chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+    return if @user
+    flash[:danger] = I18n.t("users.new.no_user")
+    redirect_to signup_path
+  end
+
   def new
-    # Do something here
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = I18n.t("users.new.welcome_new_user")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
